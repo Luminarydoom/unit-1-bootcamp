37c63f1d2d24c3ed1b6b60fe3478a8e25c1aae31
bRad-95 <bryantramirez@ac.c4q.nyc>
2018-01-02 00:21:37 -0500

    conditionals update

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -3,7 +3,37 @@
  */
 public class Conditionals {
 
-  public static void main (String args[]) {
-
+  public static void main(String args[]) {
+    System.out.println(isOdd(8));
+    isMultipleOfThree(24);
+    System.out.println("Is 24 odd and multiple of three: " + isOddAndIsMultipleOfThree(24));
+    isOddAndIsMultipleOfThree2(27);
+    fizzMultipleOfThree(27);
   }
+
+  private static void fizzMultipleOfThree(int i) {
+    if (i % 3 == 0) {
+      System.out.println("Fizz");
+    } else {
+      System.out.println(i);
+    }
+  }
+
+  private static boolean isOddAndIsMultipleOfThree2(int i) {
+    return isOdd(i) && isMultipleOfThree(i);
+  }
+
+  private static boolean isOddAndIsMultipleOfThree(int i) {
+    return i % 2 != 0 && i % 3 == 0;
+  }
+
+  private static boolean isMultipleOfThree(int i) {
+    return i % 3 == 0;
+  }
+
+  private static boolean isOdd(int i) {
+    return i % 2 != 0;
+  }
+
+
 }
